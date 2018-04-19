8cc09307991edf847f007d2c6672fdcf1522e3b2
Fredrik Lindsten <ehh.va@hotmail.com>
2018-04-19 13:29:55 +0200

    Adds bretty gud support for psmove controllers

---
+++ Assets/MoveSetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSetup : MonoBehaviour {
+
+    public static MoveSetup instance;
+    
+    public delegate void OnStart();
+    public event OnStart onStart;
+
+    public static float scale;
+    [SerializeField] GameObject masterController;
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(gameObject);
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scale = masterController.GetComponent<ControlPSMove>().CalculateScale();
+            onStart();
+        }
+    }
+}
