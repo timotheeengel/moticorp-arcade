0bede2510d9d1f3a323ecd64b29fdba7784822fb
Tim <engel.timothee@gmail.com>
2018-05-07 15:59:26 +0200

    Various Tweaks + Single Stove test scene
    ^ this

---
+++ Assets/Scripts/Refactored/SingleStoveTop.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleStoveTop : MonoBehaviour {
+
+    [SerializeField] float validationTime = 0.5f;
+    float timeOnStove = 0f;
+    bool onCorrectStove = false;
+    bool countedPanContents = false;
+    CountingPanContents pan;
+
+	// Use this for initialization
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CountingPanContents validPan = other.gameObject.GetComponentInChildren<CountingPanContents>();
+        if (validPan != null)
+        {
+            pan = validPan;
+            onCorrectStove = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CountingPanContents validPan = other.gameObject.GetComponentInChildren<CountingPanContents>();
+        if (validPan != null)
+        {
+            timeOnStove = 0f;
+            onCorrectStove = false;
+            countedPanContents = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!countedPanContents && timeOnStove >= validationTime)
+        {
+            pan.CountPanContents();
+            countedPanContents = true;
+        }
+        if (onCorrectStove)
+        {
+            timeOnStove += Time.deltaTime;
+        }
+    }
+
+}
