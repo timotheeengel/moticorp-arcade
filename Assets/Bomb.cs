4a91b3dc35d5b3c16cb9c5e7cdada2d0d4dfaa93
Tim <engel.timothee@gmail.com>
2018-05-11 11:14:15 +0200

    Bomb goes BOOM!
    ^ this

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
-    [SerializeField] int blastForce = 100;
+    [SerializeField] float blastForce = 100.0f;
     [SerializeField] float explosionRadius = 10.0f;
     [SerializeField] float timer = 3.0f;
     [SerializeField] AudioClip boomSFX;
@@ -33,39 +33,37 @@ public class Bomb : MonoBehaviour {
     {
         // TODO: Add function that sets hasTimerStarted to true after it appears on the playing area.
         timer -= Time.deltaTime;
-        if(timer <= Mathf.Epsilon)
+        if(timer <= Mathf.Epsilon && hasGoneBoom == false)
         {
             Boom();
+        }
+        else if (hasGoneBoom == true && explosionParticles.IsAlive() == false)
+        // TODO: Check particle effect in use. If children effects, then withChildren needs to be added to check no particles are alive
+        {
+            Destroy(gameObject);
         }
     }
 
     void Boom()
     {
-        if (hasGoneBoom == false)
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(boomSFX);
+        explosionParticles.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+
+        // https://docs.unity3d.com/ScriptReference/Rigidbody.AddExplosionForce.html
+        Vector3 explosionPos = GetComponent<Transform>().position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        foreach (Collider hit in colliders)
         {
-            audioSource.pitch = Random.Range(0.9f, 1.1f);
-            audioSource.PlayOneShot(boomSFX);
-            explosionParticles.Play();
-            GetComponent<MeshRenderer>().enabled = false;
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
 
-            // https://docs.unity3d.com/ScriptReference/Rigidbody.AddExplosionForce.html
-            Vector3 explosionPos = transform.position;
-            Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
-            foreach (Collider hit in colliders)
-            {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
-
-                // TODO: Add check for Food since pans have to not be affected by the explosion.
-                if (rb != null)
-                    rb.AddExplosionForce(blastForce, explosionPos, explosionRadius, 3.0F);
-            }
-
-            hasGoneBoom = true;
-            return;
-        } else if (explosionParticles.IsAlive() == false)
-            // TODO: Check particle effect in use. If children effects, then withChildren needs to be added to check no particles are alive
-        {
-            Destroy(gameObject);
+            // TODO: Add check for Food since pans have to not be affected by the explosion.
+            if (rb != null)
+                rb.AddExplosionForce(blastForce, explosionPos, explosionRadius, 3.0F, ForceMode.VelocityChange);
         }
+
+        hasGoneBoom = true;
+        return;
     }
 }
