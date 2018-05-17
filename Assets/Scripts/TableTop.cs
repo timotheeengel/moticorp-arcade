e5b494b67d053f301ffb2a68fe30400832f1ae77
Tim <engel.timothee@gmail.com>
2018-05-17 12:02:12 +0200

    Food bounces off the table
    ^ this

--- Assets/Scripts/TableTop.cs
+++ Assets/Scripts/TableTop.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TableTop : MonoBehaviour {
 
-    [SerializeField] float maxBounceOffForce = 1000000000f;
-    [SerializeField] float maxBounceOffAngle = 500000f;
+    [SerializeField] float maxBounceOffForce = 5f;
+    //[SerializeField] float maxBounceOffAngle = 50f;
     private void OnCollisionEnter(Collision collision)
     {
         GameObject collidedObj = collision.gameObject;
@@ -13,8 +13,12 @@ public class TableTop : MonoBehaviour {
         {
             Rigidbody rb = collidedObj.GetComponent<Rigidbody>();
             rb.constraints = RigidbodyConstraints.None;
-            rb.AddForce(new Vector3(Random.Range(maxBounceOffForce / 2, maxBounceOffForce), 0f, Random.Range(maxBounceOffForce / 2, maxBounceOffForce)), ForceMode.Acceleration);
-            rb.AddTorque(new Vector3 (Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), 0f), ForceMode.Acceleration);
+
+            Vector3 bounceForce = new Vector3(Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce));
+            rb.AddForce(bounceForce, ForceMode.VelocityChange);
+
+            //Vector3 bounceAngle = new Vector3(Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle));
+            //rb.AddTorque(bounceAngle, ForceMode.VelocityChange);
         }
     }
 
