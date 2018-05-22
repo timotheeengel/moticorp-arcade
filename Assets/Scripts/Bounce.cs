b500851b59bba46df803eb4f138846d2c257ef8b
Tim <engel.timothee@gmail.com>
2018-05-22 12:54:36 +0200

    Fine tuned Bounce + Boot material
    ^ this

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -7,12 +7,14 @@ public class Bounce : MonoBehaviour {
     bool hasBounce = false;
     Rigidbody rb;
     Vector3 bounceForce;
+    Vector3 bounceAngle;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void IntializeBounce(float maxBounceOffForce, float destroyDelay)
+    public void IntializeBounce(float maxBounceOffForce, float maxBounceOffAngle, float destroyDelay)
     {
         if (hasBounce == true)
         {
@@ -22,14 +24,14 @@ public class Bounce : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.None;
 
-        bounceForce = new Vector3(Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce));
+        bounceForce = new Vector3(Random.Range(-maxBounceOffForce, maxBounceOffForce), 0f, Random.Range(-maxBounceOffForce, maxBounceOffForce));
         rb.AddForce(bounceForce, ForceMode.VelocityChange);
+
+        bounceAngle = new Vector3(Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle));
+        rb.AddTorque(bounceAngle, ForceMode.VelocityChange);
 
         Destroy(gameObject, destroyDelay);
 
         hasBounce = true;
-
-        //Vector3 bounceAngle = new Vector3(Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle));
-        //rb.AddTorque(bounceAngle, ForceMode.VelocityChange);
     }
 }
--- Assets/Scripts/TableTop.cs
+++ Assets/Scripts/TableTop.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class TableTop : MonoBehaviour {
 
     [SerializeField] float maxBounceOffForce = 5f;
+    [SerializeField] float maxBounceOffAngle = 50f;
     [SerializeField] float destroyDelay = 3f;
-    //[SerializeField] float maxBounceOffAngle = 50f;
 
     private void OnCollisionEnter(Collision collision)
     {
         GameObject collidedObj = collision.gameObject;
         if (collidedObj.GetComponent<Food>() != null || collidedObj.GetComponent<Boot>() != null)
         {
-            collision.gameObject.GetComponent<Bounce>().IntializeBounce(maxBounceOffForce, destroyDelay);
+            collision.gameObject.GetComponent<Bounce>().IntializeBounce(maxBounceOffForce, maxBounceOffAngle, destroyDelay);
         }
     }
 
