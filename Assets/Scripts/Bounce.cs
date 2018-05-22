dd28b14e08af8fa3fb287d3104a0e5d0b1128565
Tim <engel.timothee@gmail.com>
2018-05-22 11:44:43 +0200

    Added recurring bounce direction after first hit
    ^ this

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+    bool hasBounce = false;
+    Rigidbody rb;
+    Vector3 bounceForce;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void IntializeBounce(float maxBounceOffForce, float destroyDelay)
+    {
+        if (hasBounce == true)
+        {
+            rb.AddForce(bounceForce, ForceMode.VelocityChange);
+            return;
+        }
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.None;
+
+        bounceForce = new Vector3(Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce));
+        rb.AddForce(bounceForce, ForceMode.VelocityChange);
+
+        Destroy(gameObject, destroyDelay);
+
+        hasBounce = true;
+
+        //Vector3 bounceAngle = new Vector3(Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle));
+        //rb.AddTorque(bounceAngle, ForceMode.VelocityChange);
+    }
+}
--- Assets/Scripts/TableTop.cs
+++ Assets/Scripts/TableTop.cs
@@ -13,16 +13,7 @@ public class TableTop : MonoBehaviour {
         GameObject collidedObj = collision.gameObject;
         if (collidedObj.GetComponent<Food>() != null || collidedObj.GetComponent<Boot>() != null)
         {
-            Rigidbody rb = collidedObj.GetComponent<Rigidbody>();
-            rb.constraints = RigidbodyConstraints.None;
-
-            Vector3 bounceForce = new Vector3(Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce), Random.Range(-maxBounceOffForce, maxBounceOffForce));
-            rb.AddForce(bounceForce, ForceMode.VelocityChange);
-
-            Destroy(collision.gameObject, destroyDelay);
-
-            //Vector3 bounceAngle = new Vector3(Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle), Random.Range(maxBounceOffAngle / 2, maxBounceOffAngle));
-            //rb.AddTorque(bounceAngle, ForceMode.VelocityChange);
+            collision.gameObject.GetComponent<Bounce>().IntializeBounce(maxBounceOffForce, destroyDelay);
         }
     }
 
