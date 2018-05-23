2b22107a7d37d06d043f0deb32a46bad0b06abc0
Tim <engel.timothee@gmail.com>
2018-05-23 13:29:05 +0200

    Recipe Zoom Out at beginning of round
    ^ this + countdown animation trigger once recipe in place.

--- Assets/Scripts/UIRecipeSwipe.cs
+++ Assets/Scripts/UIRecipeSwipe.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UISwipe : MonoBehaviour {
+public class UIRecipeSwipe : MonoBehaviour {
 
     float UIStartPosY;
     float swipeSpeed = 10f;
     float timer;
     float movementPercentage = 0f;
+    float UIStartScale;
     bool uiMoveIn = false;
     RectTransform pos;
+    Animator anim;
 
     // Use this for initialization
 	void Start () {
         pos = GetComponent<RectTransform>();
         timer = FindObjectOfType<StartCountDownAnimation>().GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length;
 
+        anim = FindObjectOfType<StartCountDownAnimation>().GetComponent<Animator>();
+
         uiMoveIn = true;
         UIStartPosY = pos.localPosition.y;
+        UIStartScale = pos.localScale.y;
 	}
 	
 	// Update is called once per frame
@@ -30,12 +35,17 @@ public class UISwipe : MonoBehaviour {
 
     void SwipeDown()
     {
-        if (pos.localPosition.y > 0)
+        if (pos.localPosition.y < 0)
         {
             pos.localPosition = new Vector3(0f, Mathf.Lerp(UIStartPosY, 0f, movementPercentage));
+            float newScale = Mathf.Lerp(UIStartScale, 1f, movementPercentage);
+            pos.localScale = new Vector3(newScale, newScale, 1f);
             movementPercentage += Time.deltaTime;
         }
         else
+        {
             uiMoveIn = false;
+            anim.SetTrigger("StartCountdown");
+        }
     }
 }
